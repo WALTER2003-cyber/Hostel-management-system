26f9df68062cc536f9fd3a493925e8f27226e0d7
WALTER003-cyber <walterwalts3m@gmail.com>
2026-07-25 18:32:05 +0300

    class user created

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,6 @@
 <?php
 include "db.php";
+include "users.php";
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn= new mysqli("localhost","root","@mike03!","hostelmanagement",3306);
 //grabbing input from login page
@@ -20,8 +21,14 @@ try{
 }catch(mysqli_sql_exception $e){
     die ("Error occured").$e->getMessage();
 }
-$_SESSION['userid'] = $row['uuid'];
+$user = new User($row['f_name'],$row['m_name'],$row['l_name']);
+$_SESSION['user'] = $user;
+$user->getf_name();
+//NB:
+//to access the functions of user class, you have to call session and assign to a variable eg
+//$users = $_SESSION['user'];
 //creating variables for retrieved data
+//by doing this, you are able to call the values being stored inside session
     $Email=$row['email'] ?? NULL;
     $pass=$row['password'] ?? NULL;
     $firstname=$row['f_name'] ?? NULL;
